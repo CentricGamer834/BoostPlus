bd66275bee03b5fd221c7c1d3292863b015c3abc
Centric <theminecrafter13772@gmail.com>
2024-12-23 21:38:20 -0500

    wow
    wow

--- main.js
+++ main.js
@@ -2,25 +2,34 @@ function carouseReviews() {
     const previousBtn = document.getElementById("previous");
     const nextBtn = document.getElementById("next");
     const reviewImg = document.getElementById("review");
+    const maxReviews = 3;
     let curReview = 0;
 
-    previousBtn.addEventListener("click", () => {
-        curReview = --curReview;
+    const updateReviewImage = () => {
         reviewImg.src = `./assets/images/reviews/${curReview}.png`;
+        previousBtn.disabled = curReview === 0;
+        nextBtn.disabled = curReview === maxReviews - 1;
+    };
+
+    previousBtn.addEventListener("click", () => {
+        if (curReview > 0) {
+            curReview--;
+            updateReviewImage();
+        }
     });
 
     nextBtn.addEventListener("click", () => {
-        curReview = ++curReview;
-        reviewImg.src = `./assets/images/reviews/${curReview}.png`;
+        if (curReview < maxReviews - 1) {
+            curReview++;
+            updateReviewImage();
+        }
     });
+
+    updateReviewImage();
 }
 
 function redirect() {
-    location.href = "https://discord.gg/RUBwVP9B57";
+    window.open("https://discord.gg/RUBwVP9B57", "_blank");
 }
 
-function main() {
-    carouseReviews();
-}
-
-window.onload = main;
+window.onload = carouseReviews;
